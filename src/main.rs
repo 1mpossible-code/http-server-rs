15e3b86809b4ae8f4f33a38d012a62530b268a78
Max <linme00p@gmail.com>
2024-06-14 21:14:30 -0400

    print request to terminal

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
@@ -6,6 +7,16 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established")
+        handle_connection(stream)
+    }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let reader = BufReader::new(stream);
+    let mut lines = reader.lines();
+
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        println!("{}", line);
     }
 }
