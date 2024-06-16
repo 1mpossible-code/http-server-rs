ecb0525cdb6988e3dbcd92101ed3b6fa61806d40
Max <linme00p@gmail.com>
2024-06-15 23:54:04 -0400

    print the uri of request

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
-use std::{io::{BufRead, BufReader, Write}, net::TcpStream};
+use std::{
+    fmt::format,
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+};
 
+struct Route {
+    path: String,
+    body: String,
+}
 
-pub fn handle_connection(mut stream: TcpStream) {
-    let reader = BufReader::new(&stream);
-    let mut lines = reader.lines().map(|l| l.unwrap()).take_while(|l| l != "");
+pub struct Controller {
+    routes: Vec<Route>,
+}
 
-    while let Some(line) = lines.next() {
-        println!("{}", line);
+impl Route {
+    pub fn new(path: String, body: String) -> Route {
+        Route { path, body }
+    }
+}
+
+impl Controller {
+    pub fn new() -> Controller {
+        Controller { routes: Vec::new() }
     }
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    pub fn add_route(&mut self, path: &str, body: &str) {
+        self.routes
+            .push(Route::new(path.to_string(), body.to_string()));
+    }
 
-    // Send html file as response
-    let html = include_str!("test.html");
-    let response = format!("{}{}", response, html);
+    pub fn handle_connection(&self, mut stream: TcpStream) {
+        let reader = BufReader::new(&stream);
+        let mut lines = reader.lines().next().unwrap().unwrap();
+        let path = format!("{}", lines.split_whitespace().nth(1).unwrap());
 
+        println!("path: {}", path);
 
-    stream.write(response.as_bytes()).unwrap();
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        stream.write(response.as_bytes()).unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::net::TcpListener;
 
-
-use http_server_rs::handle_connection;
+use http_server_rs::Controller;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
 
+    let mut controller = Controller::new();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream)
+        controller.handle_connection(stream);
     }
 }
