903f7684ddfe612252f7e9ac852ae53993ebefbc
Max <linme00p@gmail.com>
2024-06-19 21:05:26 -0400

    change vector to hashmap to handle routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,22 @@
 use std::{
+    collections::HashMap,
     io::{BufRead, BufReader, Write},
     net::TcpStream,
 };
 
-struct Route {
-    path: String,
-    body: String,
-}
-
 pub struct Controller {
-    routes: Vec<Route>,
-}
-
-impl Route {
-    pub fn new(path: String, body: String) -> Route {
-        Route { path, body }
-    }
+    routes: HashMap<String, String>,
 }
 
 impl Controller {
     pub fn new() -> Controller {
-        Controller { routes: Vec::new() }
+        Controller {
+            routes: HashMap::new(),
+        }
     }
 
     pub fn add_route(&mut self, path: &str, body: &str) {
-        self.routes
-            .push(Route::new(path.to_string(), body.to_string()));
+        self.routes.insert(path.to_string(), body.to_string());
     }
 
     pub fn handle_connection(&self, mut stream: TcpStream) {
@@ -48,14 +39,13 @@ impl Controller {
     }
 
     fn get_content_body(&self, path: &str) -> String {
-        for route in &self.routes {
-            if route.path == path {
-                if route.body.ends_with(".html") {
-                    let body = std::fs::read_to_string(&route.body).unwrap();
-                    return body;
-                }
-                return route.body.clone();
+        if self.routes.contains_key(path) {
+            let route = self.routes.get(path).unwrap();
+            if route.ends_with(".html") {
+                let body = std::fs::read_to_string(&route).unwrap();
+                return body;
             }
+            return route.clone();
         }
 
         String::from("404 Not Found")
